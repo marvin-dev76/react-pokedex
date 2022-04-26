a3c8da1a0101664e341eb11b739d220b2bf18ef6
Marvin Orellana <marvin-dev76@gmail.com>
2022-04-25 20:54:43 -0600

    deploying

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Pokemons from './components/Pokemons/Pokemons';
 import PokeSpecific from './components/Pokemons/PokeSpecific/PokeSpecific';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="background light">
-        <BrowserRouter>
+        <HashRouter>
           <Navbar />
           <Routes>
             <Route element={<Pokemons />} path="/" exact />
             <Route element={<PokeSpecific />} path="/pokemon/:name" exact />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </Provider>
   );
